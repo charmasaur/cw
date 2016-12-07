1391318ef38519033644ce55c93a5d8bba444643
Harry Slatyer <harry.slatyer@gmail.com>
2016-12-07 22:45:54 +1100

    Replace blur with FFT

--- filter_test.cc
+++ filter_test.cc
@@ -8,12 +8,37 @@ using namespace cv;
 using namespace std;
 
 void filter(Mat& input, Mat& output) {
-    Mat kernel = (Mat_<double>(4,4) << 0, 0.1, 0.1, 0,
-                                     0.1, 0.05, 0.05, 0.1,
-                                     0.1, 0.05, 0.05, 0.1,
-                                     0, 0.1, 0.1, 0);
+    Mat& padded = input;
+    int m = getOptimalDFTSize(input.rows);
+    int n = getOptimalDFTSize(input.cols);
+    copyMakeBorder(input, padded, 0, m - input.rows, 0, n - input.cols, BORDER_CONSTANT, Scalar::all(0));
+    Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+    Mat complexI;
+    merge(planes, 2, complexI);
+    dft(complexI, complexI);
+    split(complexI, planes);
+    magnitude(planes[0], planes[1], planes[0]);
+    Mat magI = planes[0];
+    magI += Scalar::all(1);
+    log(magI, magI);
+    magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
+    int cx = magI.cols/2;
+    int cy = magI.rows/2;
+    Mat q0(magI, Rect(0, 0, cx, cy));
+    Mat q1(magI, Rect(cx, 0, cx, cy));
+    Mat q2(magI, Rect(0, cy, cx, cy));
+    Mat q3(magI, Rect(cx, cy, cx, cy));
 
-    filter2D(input, output, input.depth(), kernel);
+    Mat tmp;
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+
+    normalize(magI, output, 0, 1, CV_MINMAX);
 }
   
 
