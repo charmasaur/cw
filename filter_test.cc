23a0c9cc883eb2abe0de94f228c42740723a254a
Harry Slatyer <harry.slatyer@gmail.com>
2016-12-10 01:01:02 +1100

    Add basic rotation stuff
    Tried a Hough transform but it didn't work so well. Should probably try
    this again though because apparently it should work for this kind of
    thing. Other good option could be PCA. In any case, for now I've just
    done a silly bucketing thing with the angles. It seems to do moderately
    well.

--- filter_test.cc
+++ filter_test.cc
@@ -3,6 +3,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace cv;
 using namespace std;
@@ -38,7 +39,78 @@ void filter(Mat& input, Mat& output) {
     q2.copyTo(q1);
     tmp.copyTo(q2);
 
-    normalize(magI, output, 0, 1, CV_MINMAX);
+    Mat temp_output;
+    normalize(magI, temp_output, 0, 1, CV_MINMAX);
+
+    temp_output.convertTo(temp_output, CV_8UC1, 255.);
+
+    Mat thresholded;
+    threshold(temp_output, thresholded, 128., 255., THRESH_BINARY);
+
+    // Calculate rotation.
+    int midr = thresholded.rows / 2;
+    int midc = thresholded.cols / 2;
+    int nbuckets = 100;
+    int buckets[nbuckets];
+    double total_angle_in_buckets[nbuckets];
+    int best_bucket = 0;
+    for (int i = 0; i < nbuckets; ++i) {
+            buckets[i] = 0;
+            total_angle_in_buckets[i] = 0.;
+    }
+    for (int r = 0; r < thresholded.rows; r++) {
+        for (int c = 0; c < thresholded.cols; c++) {
+            if ((int) thresholded.at<uchar>(r, c) < 128) {
+                continue;
+            }
+            double angle = atan2(midr - r, midc - c);
+            // We only care about the angles between -pi/4 and pi/4 (assume image is almost
+            // upright).
+            int angle_bucket = int(((angle * 2. / 3.14) + 0.5) * double(nbuckets));
+            if (angle_bucket < 0 || angle_bucket >= nbuckets) {
+                    continue;
+            }
+            total_angle_in_buckets[angle_bucket] += angle;
+            if (++buckets[angle_bucket] > buckets[best_bucket]) {
+                    best_bucket = angle_bucket;
+            }
+        }
+    }
+    // This seems OK, but the problem is that the bucket positioning could have a big effect. I
+    // think something like PCA would be more sensible. Evan also thought Hough might actually work
+    // if done properly.
+    double rot_angle_rad = total_angle_in_buckets[best_bucket] / double(buckets[best_bucket]);
+    double rot_angle_deg = rot_angle_rad * 180. / 3.14;
+    cout << "Rotation angle (degrees): " << rot_angle_deg << endl;
+
+    Mat rot = getRotationMatrix2D(Point(midc, midr), rot_angle_deg, 1.f);
+    Mat rotated;
+    warpAffine(input, rotated, rot, input.size());
+
+    output = rotated;
+
+    // Don't think Hough is quite right, since they're not real lines. Maybe PCA? Or we could just
+    // do a histogram of all angles...
+    //vector<Vec2f> lines;
+    //HoughLines(temp_output, lines, 10, CV_PI/180, 10000, 0, 0);
+    //for (size_t i = 0; i < lines.size(); ++i) {
+    //    float rho = lines[i][0];
+    //    float theta = lines[i][1];
+    //    Point pt1, pt2;
+    //    double a = cos(theta), b = sin(theta);
+    //    double x0 = a*rho, y0 = b*rho;
+    //    pt1.x = cvRound(x0 + 1000*(-b));
+    //    pt1.y = cvRound(y0 + 1000*a);
+    //    pt2.x = cvRound(x0 - 1000*(-b));
+    //    pt2.y = cvRound(y0 - 1000*a);
+    //    line(output, pt1, pt2, Scalar(0, 0, 255), 1, CV_AA);
+
+    //}
+    //Mat kernel = (Mat_<double>(4,4) << 0, 0.1, 0.1, 0,
+    //                                   0.1, 0.05, 0.05, 0.1,
+    //                                   0.1, 0.05, 0.05, 0.1,
+    //                                   0, 0.1, 0.1, 0);
+    //filter2D(temp_output, output, temp_output.depth(), kernel);
 }
   
 
@@ -60,9 +132,7 @@ int main(int argc, char **argv) {
     filter(input, output);
     namedWindow("Display window", WINDOW_AUTOSIZE);
     imshow("Display window", output);
-    Mat img8bit;
-    output.convertTo(img8bit, CV_8UC3, 255.);
-    imwrite(string("output_") + string(argv[1]), img8bit);
+    imwrite(string("output_") + string(argv[1]), output);
 
     waitKey(0);
     return 0;
