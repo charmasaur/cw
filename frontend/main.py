875b5a1b3528bd2cba92c652af088a3a591c5672
Harry Slatyer <harry.slatyer@gmail.com>
2017-01-23 22:21:03 +1100

    Display uploaded image, and do a dummy CW fetch

--- frontend/main.py
+++ frontend/main.py
@@ -1,4 +1,6 @@
 from flask import Markup, Flask, redirect, request, render_template
+from google.appengine.api import urlfetch
+import base64
 
 app = Flask(__name__)
 
@@ -11,5 +13,19 @@ def go():
     if 'image_file' not in request.files:
         return redirect('/')
     file = request.files['image_file']
-    return file.filename
+    ext = file.filename.split(".")[-1]
+
+    # TODO: Talk to the actual server
+    data = urlfetch.fetch(
+            url="http://localhost:8080/tmpdata",
+            payload="hello there!",
+            method=urlfetch.POST,
+            headers={'Content-Type': 'text/html'}).content
+
+    # TODO: Render the actual UI
+    return '<html><body>' + data + '<br><img src="data:image/' + ext + ';base64,' + base64.b64encode(file.read()) + '"/></body></html>'
 #http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+
+@app.route('/tmpdata', methods=['POST'])
+def tmp_data():
+    return "You sent: " + request.get_data()
