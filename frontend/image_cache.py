6e72b0a54ab9c00f767ca69eacfeb692ef07509d
Harry Slatyer <harry.slatyer@gmail.com>
2017-03-02 22:00:55 +1100

    Add optimisation TODO to image cache

--- frontend/image_cache.py
+++ frontend/image_cache.py
@@ -9,6 +9,9 @@ def put(bdata, ext):
     return item.put().urlsafe()
 
 def get(key):
+    # TODO: Make my own keys, so I can do deduping stuff.. or alternatively
+    # include an indexed token in the model, and search for that when adding
+    # new entries
     item = ndb.Key(urlsafe=key).get()
     if not item:
         return None
