f825b2fd42f1d9b4258d3da19244d84e22210ca9
Harry Slatyer <harry.slatyer@gmail.com>
2021-01-26 17:40:16 +1100

    Start migrating image cache

--- appengine/app/image_cache.py
+++ appengine/app/image_cache.py
@@ -1,11 +1,17 @@
-from google.appengine.ext import ndb
+import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+from app.app import db
+
+
 # TODO
 
 class ImageDataItem(ndb.Model):
-    bdata = ndb.StringProperty(indexed=False)
-    ext = ndb.StringProperty()
-    cw_data = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
+    key = ... something automatic
+    bdata = db.Column(db.String)
+    ext = db.Column(db.String)
+    cw_data = db.Column(db.String)
 
 def delete(key):
     item = ndb.Key(urlsafe=key).get()
@@ -34,3 +40,29 @@ def get(key):
 def get_recents(count):
     results = ImageDataItem.query().order(-ImageDataItem.date).fetch(count)
     return [(result.date, result.key.urlsafe()) for result in results]
+
+class UserSavedCrosswordData(db.Model):
+    uid = db.Column(db.String, primary_key=True)
+    cw_key = db.Column(db.String, primary_key=True)
+    data = db.Column(db.String)
+
+def _get_item(uid, cw_key):
+    return UserSavedCrosswordData.query.get(
+        {"uid": uid, "cw_key": cw_key})
+
+# Does just about what you'd expect. cw_key has to uniquely identify the crossword (that is, it's
+# just a dumb key, it's up to the caller to choose a useful type).
+def put(uid, cw_key, data):
+    item = _get_item(uid, cw_key)
+    if not item:
+        # Add a new item
+        item = UserSavedCrosswordData(uid=uid, cw_key=cw_key, data="")
+        db.session.add(item)
+    item.data = data
+    db.session.commit()
+
+def get(uid, cw_key):
+    item = _get_item(uid, cw_key)
+    if not item:
+        return None
+    return item.data
